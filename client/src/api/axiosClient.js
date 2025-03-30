77a976ee7271fcd66a74b5b1f701b2190730ff0a
Ayush <ayushchaurasiya289@gmail.com>
2025-03-30 17:03:03 +0530

    assignment-task

--- client/src/api/axiosClient.js
+++ client/src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import queryString from 'query-string'
 
-const baseUrl = 'https://prodios-labs-assignment-mxtx.vercel.app/'
+const baseUrl = 'https://prodios-labs-assignment-mxtx.vercel.app/api/v1/'
 const getToken = () => localStorage.getItem('token')
 
 const axiosClient = axios.create({
